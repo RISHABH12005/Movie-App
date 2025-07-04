e041fada7d6e71bc3bed39678af44bfafcd1930c
RISHABH12005 <2r10j5@gmail.com>
2025-07-05 02:18:24 +0530

    Initial commit of Android project

---
+++ app/src/main/java/com/example/movie/MainActivity.kt
@@ -0,0 +1,59 @@
+package com.example.movie
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.OnBackPressedCallback
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.viewinterop.AndroidView
+import com.example.movie.ui.theme.MovieTheme
+
+class MainActivity : ComponentActivity() {
+
+    private lateinit var webView: WebView
+
+    @SuppressLint("SetJavaScriptEnabled")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        onBackPressedDispatcher.addCallback(this, object : OnBackPressedCallback(true) {
+            override fun handleOnBackPressed() {
+                if (::webView.isInitialized && webView.canGoBack()) {
+                    webView.goBack()
+                } else {
+                    finish()
+                }
+            }
+        })
+
+        setContent {
+            MovieTheme{
+                WebViewScreen(
+                    url = "https://bollyflix.dance",
+                    onWebViewReady = { webView = it }
+                )
+            }
+        }
+    }
+}
+
+@Composable
+fun WebViewScreen(url: String, onWebViewReady: (WebView) -> Unit) {
+    AndroidView(
+        modifier = Modifier.fillMaxSize(),
+        factory = { context ->
+            WebView(context).apply {
+                webViewClient = WebViewClient()
+                settings.javaScriptEnabled = true
+                settings.domStorageEnabled = true
+                loadUrl(url)
+                onWebViewReady(this)
+            }
+        }
+    )
+}
